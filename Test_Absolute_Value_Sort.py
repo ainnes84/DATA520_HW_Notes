388ee48a62d6319b26244af1a704e3c75b40cef6
Andrew Innes <ainnes84@live.com>
2017-11-15 14:28:08 -0500

    Fixed Homework 15

--- Test_Absolute_Value_Sort.py
+++ Test_Absolute_Value_Sort.py
@@ -1,37 +1,56 @@
 import unittest
-import average as avg
- 
-class TestAverage(unittest.TestCase):
-    """Tests for average."""
- 
-    def test_contains_none(self):
-        """Test a list of values that contains None."""
-        argument = avg.average([25,30,None,35])
-        expected = 30
-        self.assertEqual(expected, argument, "The list of values contains None.")
- 
-    def test_dups(self):
-        """Test a list with duplicates."""
-        argument = avg.average([25,38,25,30,30])
-        expected = 29.6
-        self.assertEqual(expected, argument, "The list has duplicates.")
- 
-    def test_one_item(self):
-        """Test a list that contains one item."""
-        argument = avg.average([25])
-        expected = 25
-        self.assertEqual(expected, argument, "The list has one item.")
-   
-    def test_empty_list(self):
-        """Test an empty list."""
-        argument = avg.average([])
-        expected = None
-        self.assertEqual(expected, argument, "The list has no items.")
- 
-    def test_normal_with_none(self):
-        """Test a list that only contains None."""
-        argument = avg.average([None])
-        expected = None
-        self.assertEqual(expected, argument, "The list contains only missing values.")
- 
+import absolute_value_sort as abs_val
+class TestAbsoluteValueSort(unittest.TestCase):
+    """Tests for absolute_value_sort"""
+
+    def test_abs_val_empty(self):
+        """Test an empty list"""
+        argument = []
+        expected = []
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list is empty.")
+
+    def test_abs_val_one_item(self):
+        """Test one-item list."""
+        argument = [5]
+        expected = [5]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contains one item.")
+
+    def test_abs_val_two_items(self):
+        """Test a two-item list."""
+        argument = [2, 5]
+        expected = [2, 5]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contains two items.")
+
+    def test_abs_val_multi_zeros(self):
+        """Test a list of zeros."""
+        argument = [0, 0, 0, 0]
+        expected = [0, 0, 0, 0]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contains two items.")
+
+    def test_abs_val_multi_neg(self):
+        """Test a list of negative values."""
+        argument = [-1, -5, -3, -4]
+        expected = [-1, -3, -4, -5]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contains only negative values.")
+
+    def test_abs_val_multi_pos(self):
+        """Test a list of positive values."""
+        argument = [4, 2, 3, 6]
+        expected = [2, 3, 4, 6]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contians only positive values.""")
+
+    def test_abs_val_multi_mix(self):
+        """Test a list containing mixture of negative values, zeros, and positive values."""
+        argument = [4, 0, 2, -5, 0]
+        expected = [0, 0, 2, 4, -5]
+        abs_val.absolute_value_sort(argument)
+        self.assertEqual(expected, argument, "The list contains a mixture of negative values, zeros, and positive values.")
+        
 unittest.main()
+
